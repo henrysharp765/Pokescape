f4be152b76da768db1160cba68010386256f9ab4
Henry Sharp <henry.sharp22@gmail.com>
2024-03-18 11:13:00 +0000

    Uploading changes to ui layout

---
+++ Pokescape-master (2)/Pokescape-master/PokescapeServer/GridCoordinate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokescapeServer
+{/**
+    enum GridCoordinateType
+    {
+        OpenGroundStone,
+        WallStone,
+        CavePassage
+    }**/
+    public class GridCoordinate
+    {
+        public bool CanWalkThrough;
+        public string ImageSource;
+        public bool CanStorePokemon;
+       
+    }
+    public class Wallstone: GridCoordinate
+    {
+        public Wallstone()
+        {
+            ImageSource = "../Image/blockImages/wallImage.jpg";
+            CanStorePokemon = false;
+            CanWalkThrough = false;
+            //set imageSource here
+        }
+    }
+    public class OpenGroundStone: GridCoordinate
+    {
+        
+        public OpenGroundStone()
+        {
+            ImageSource = "../Image/blockImages/pathImage.png";
+            CanStorePokemon = true;
+            CanWalkThrough = true;
+        }
+    }
+}
---
+++ Pokescape-master (2)/Pokescape-master/PokescapeServer/ScapeMonster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokescapeServer
+{
+    public class ScapeMonsterMove
+    {
+        public int MoveDamage;
+        public string Name;
+    }
+    public enum ScapeMonsterType
+    {
+        Fire, 
+        Water,
+        Rock
+    }
+    public class ScapeMonster
+    {
+        public (int xCord, int yCord) PokemonCoordinates;
+        public string ScapeMonsterName;
+        public string ScapeMonsterId;
+        public ScapeMonsterType Type;
+        public double MaximumHealth;
+        public double Health;
+        public double DamagePerHit;
+        public List<ScapeMonsterMove> Moves;
+        public int Level; 
+
+        public ScapeMonster() { }   
+
+    }
+}
